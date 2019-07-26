3c72a86d7452753ad22a72559efeee58d5f831df
Jerry Doran <Jerry.Doran@pccairfoils.com>
2019-07-26 09:50:50 -0400

    Initial Project Setup

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,9 @@
 const express = require('express');
 const router = express.Router();
 
-// SET UP OUR GET TO THE HOME ROUTE
+// SET UP OUR 'GET' FOR THE HOME ROUTE: LOCAL HOST 3000.
 router.get('/', (req, res) => {
+  // Take the 'index.ejs' file and put it into the 'layouts.ejs' file and display to client browser
   res.render('index');
 });
 
--- server.js
+++ server.js
@@ -1,17 +1,35 @@
+// if (process.env.NODE_ENV !== 'production') {
+//   require('dotenv').parse();
+// }
+
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const indexRouter = require('./routes/index');
+const mongoose = require('mongoose');
 
 const app = express();
 
+// CONNECT TO MONGODB DATABASE
+// mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+mongoose.connect('mongodb://localhost:27017/mybrary', {
+  useNewUrlParser: true
+});
+const db = mongoose.connection;
+db.on('error', error => console.log(error));
+db.once('open', () => console.log('Connected to MongoDB'));
+
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
+
+// Every single '.ejs' file will be put into the 'layout.ejs' file template
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 
 // Where I will store css style sheets, images, js files, etc.
 app.use(express.static('public'));
 
+// This indexRouter variable will be set to the 'router' variable that is exported
+// from the 'index.js' file
 app.use('/', indexRouter);
 
 let PORT = process.env.PORT || 3000;
